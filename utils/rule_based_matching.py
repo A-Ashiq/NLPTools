a1f474415d134fa2e48e7a0807a991075567545f
afaanashiq <afaan.ashiq2@gmail.com>
2020-12-12 17:13:46 +0000

    Refactor of rule-based pattern matching

--- utils/rule_based_matching.py
+++ utils/rule_based_matching.py
@@ -4,12 +4,27 @@ from spacy.matcher import Matcher
 from utils.nlp_document_parsing import NLP, open_as_nlp_document
 
 
-def extract_full_names(nlp_document: Doc) -> list[str]:
-    pattern = [{"POS": "PROPN"}, {"POS": "PROPN"}]
-    matcher = Matcher(NLP.vocab)
-    matcher.add("FULL_NAME", None, pattern)
-    nlp_document = open_as_nlp_document(nlp_document)
+RULE_BASED_PATTERNS = {
+    "FULL NAME": [{"POS": "PROPN"}, {"POS": "PROPN"}],
+    "PHONE NUMBER": [{'ORTH': '+'}, {'SHAPE': 'ddd'},]
+}
 
+
+def _extract_entity(entity, nlp_document) -> list[str]:
+    entity = entity.upper()
+    pattern = RULE_BASED_PATTERNS.get(entity)
+    matcher = Matcher(NLP.vocab)
+    matcher.add(entity, None, pattern)
+
+    nlp_document = open_as_nlp_document(nlp_document)
     matches = matcher(nlp_document)
 
     return [nlp_document[start:end].text for _, start, end in matches]
+
+
+def extract_full_names(nlp_document: Doc) -> list[str]:
+    return _extract_entity(entity="FULL NAME", nlp_document=nlp_document)
+
+
+def extract_phone_number(nlp_document):
+    return _extract_entity("PHONE NUMBER", nlp_document)
